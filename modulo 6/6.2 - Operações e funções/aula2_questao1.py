6b57fc4e43c34405c6d71f26714cf0275346ee18
nielpd <daniel.batista@pditabira.com>
2024-10-22 17:40:44 -0300

    up

--- modulo 6/6.2 - Operações e funções/aula2_questao1.py
+++ modulo 6/6.2 - Operações e funções/aula2_questao1.py
@@ -1,16 +1,11 @@
 import random
+Lista = [] 
 
-def GerarLista():
-    Lista = [] 
-    for i in range(20):
-        num = random.randint(-100, 100)
-        Lista.append(num)
+for i in range(20):
+  num = random.randint(-100, 100)
+  Lista.append(num)
 
-    return Lista
-
-ListaGerada = GerarLista()
-
-print("Lista gerada:  ", ListaGerada)
-print("Lista ordenada:  ", sorted(ListaGerada))
-print("Maior indice: ", max(enumerate(ListaGerada), key=lambda x: x[1]))
-print("Menor indice: ", min(enumerate(ListaGerada), key=lambda x: x[1]))
+print("Lista ordenada:  ", sorted(Lista))
+print("Lista gerada:  ", Lista)
+print("Maior indice: ", Lista.index(max(Lista)))
+print("Menor indice: ", Lista.index(min(Lista)))
